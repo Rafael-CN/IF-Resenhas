823fd1a31c6aa6543e6220fd42bc266d6b4bde36
Rafael <rcn.ytay+work@gmail.com>
2024-12-09 15:45:45 -0300

    login screen fix

--- js/style.js
+++ js/style.js
@@ -1,131 +1,135 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  // Login
-  containerSuperiorAvatar: {
-    backgroundColor: "rgba(173, 126, 148, 0.4)",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 60,
-    paddingHorizontal: 20,
-    marginBottom: 20,
-    width: "100%",
-    position: "absolute",
-    top: 0,
-  },
-  avatarLogo: {
-    width: 140,
-    height: 140,
-    marginBottom: 10,
-  },
-  titleApp: {
-    fontSize: 28,
-    fontFamily: "serif",
-    color: "#fff",
-    fontWeight: "bold",
-  },
+    // Login
+    formContainer: {
+        width: "100%",
+        paddingBottom: 70,
+        paddingTop: 20,
+        alignItems: "center",
+        backgroundColor: "#FED2E5",
+    },
 
-  inputContainer: {
-    marginTop: 250,
-    width: "80%",
-  },
-  input: {
-    backgroundColor: "rgba(173, 126, 148, 0.3)",
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 8,
-    marginTop: 4,
-    borderWidth: 2,
-    borderColor: "#CD9CB2",
-    color: "#fff",
-  },
-  inputPlaceholder: {
-    color: "#CD9CB2",
-  },
-  inputLabel: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#AD7E94",
-  },
+    containerSuperiorAvatar: {
+        backgroundColor: "#DDB0C4",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 60,
+        paddingHorizontal: 20,
+        width: "100%",
+    },
+    avatarLogo: {
+        width: 140,
+        height: 140,
+        marginBottom: 10,
+    },
+    titleApp: {
+        fontSize: 28,
+        fontFamily: "serif",
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    inputContainer: {
+        marginTop: 10,
+        width: "80%",
+    },
+    input: {
+        backgroundColor: "rgba(173, 126, 148, 0.3)",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 8,
+        marginTop: 4,
+        borderWidth: 2,
+        borderColor: "#CD9CB2",
+        color: "#fff",
+    },
+    inputPlaceholder: {
+        color: "#CD9CB2",
+    },
+    inputLabel: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#AD7E94",
+    },
+    buttonContainer: {
+        width: "80%",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 30,
+    },
+    button: {
+        backgroundColor: "#8F6277",
+        width: "100%",
+        padding: 15,
+        borderRadius: 50,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "#FFFFFF",
+        fontWeight: "Bold",
+        fontSize: 16,
+    },
+    registerText: {
+        marginTop: 15,
+        textAlign: "center",
+        fontSize: 14,
+        color: "#AD7E94",
+    },
+    registerLink: {
+        color: "#8F6277",
+        fontWeight: "bold",
+    },
+    distancia: {
+        marginTop: 15,
+    },
+    logoutIconContainer: {
+        position: "absolute",
+        top: 20,
+        right: 20,
+        zIndex: 1,
+    },
+    // prof
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#FED2E5",
+    },
+    containerCenter: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#DDB0C4",
+    },
 
-  buttonContainer: {
-    width: "80%",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 100,
-  },
-  button: {
-    backgroundColor: "#8F6277",
-    width: "100%",
-    padding: 15,
-    borderRadius: 50,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "#FFFFFF",
-    fontWeight: "Bold",
-    fontSize: 16,
-  },
-  registerText: {
-    marginTop: 15,
-    textAlign: "center",
-    fontSize: 14,
-    color: "#AD7E94",
-  },
-  registerLink: {
-    color: "#8F6277",
-    fontWeight: "bold",
-  },
-  distancia: {
-    margimTop: 30,
-  },
-  logoutIconContainer: {
-    position: "absolute",
-    top: 20,
-    right: 20,
-    zIndex: 1,
-  },
-  // prof
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-  containerCenter: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#FED2E5",
-  },
-
-  //FLATLIST
-  item: {
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "#FFE1EE",
-    borderRadius: 10,
-    padding: 10,
-    marginVertical: 8,
-    width: "90%",
-    // alignSelf: "center",
-    borderColor: "#CD9CB2D9",
-    borderWidth: 2,
-  },
-  titulo: {
-    fontSize: 14,
-    color: "#8F6277",
-    marginBottom: 4,
-    flexWrap: "wrap",
-  },
-  imagem: {
-    width: 60,
-    height: 60,
-    borderRadius: 8,
-    backgroundColor: "#AD7E944D",
-    marginTop: 15,
-  },
-  imagemView: {
-    alignContent: "center",
-    alignItems: "center",
-    marginBottom: 40,
-  },
+    //FLATLIST
+    item: {
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: "#FFE1EE",
+        borderRadius: 10,
+        padding: 10,
+        marginVertical: 8,
+        width: "90%",
+        // alignSelf: "center",
+        borderColor: "#CD9CB2D9",
+        borderWidth: 2,
+    },
+    titulo: {
+        fontSize: 14,
+        color: "#8F6277",
+        marginBottom: 4,
+        flexWrap: "wrap",
+    },
+    imagem: {
+        width: 60,
+        height: 60,
+        borderRadius: 8,
+        backgroundColor: "#AD7E944D",
+        marginTop: 15,
+    },
+    imagemView: {
+        alignContent: "center",
+        alignItems: "center",
+        marginBottom: 40,
+    },
 });
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,84 +1,66 @@
-import {
-  KeyboardAvoidingView,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Image,
-} from "react-native";
+import { KeyboardAvoidingView, View, Text, TextInput, TouchableOpacity, Image, ImageStyle } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { auth } from "../js/firebase";
 import style from "../js/style";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
 
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      if (user) navigation.navigate("Menu" as never);
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) navigation.navigate("Menu" as never);
+        });
     });
-  });
 
-  const irParaRegistro = () => {
-    navigation.navigate("Registro" as never);
-  };
+    const irParaRegistro = () => {
+        navigation.navigate("Registro" as never);
+    };
 
-  const handleLogin = () => {
-    auth
-      .signInWithEmailAndPassword(email, senha)
-      .then((userCredentials) => {
-        const user = userCredentials.user;
-        console.log("Logado como ", user.email);
-      })
-      .catch((error) => alert(error.message));
-  };
+    const handleLogin = () => {
+        auth.signInWithEmailAndPassword(email, senha)
+            .then((userCredentials) => {
+                const user = userCredentials.user;
+                console.log("Logado como ", user.email);
+            })
+            .catch((error) => alert(error.message));
+    };
 
-  return (
-    <KeyboardAvoidingView style={style.containerCenter}>
-      <View style={style.containerSuperiorAvatar}>
-        <Image
-          source={require("../assets/avatar.png")}
-          style={style.avatarLogo}
-        />
-        <Text style={style.titleApp}>CineFy</Text>
-      </View>
+    return (
+        <KeyboardAvoidingView style={style.containerCenter}>
+            <View style={style.containerSuperiorAvatar}>
+                <Image source={require("../assets/avatar.png")} style={style.avatarLogo as ImageStyle} />
+                <Text style={style.titleApp}>CineFy</Text>
+            </View>
 
-      <View style={style.inputContainer}>
-        <View style={style.distancia}>
-          <Text style={style.inputLabel}>Email:</Text>
-          <TextInput
-            placeholder="Email"
-            onChangeText={(texto) => setEmail(texto)}
-            style={style.input}
-          />
-        </View>
+            <View style={style.formContainer}>
+                <View style={style.inputContainer}>
+                    <View style={style.distancia}>
+                        <Text style={style.inputLabel}>Email:</Text>
+                        <TextInput placeholder="Email" onChangeText={(texto) => setEmail(texto)} style={style.input} />
+                    </View>
 
-        <View style={style.distancia}>
-          <Text style={style.inputLabel}>Senha:</Text>
-          <TextInput
-            placeholder="Senha"
-            onChangeText={(texto) => setSenha(texto)}
-            style={style.input}
-            secureTextEntry
-          />
-        </View>
-      </View>
+                    <View style={style.distancia}>
+                        <Text style={style.inputLabel}>Senha:</Text>
+                        <TextInput placeholder="Senha" onChangeText={(texto) => setSenha(texto)} style={style.input} secureTextEntry />
+                    </View>
+                </View>
 
-      <View style={style.buttonContainer}>
-        <TouchableOpacity style={style.button} onPress={handleLogin}>
-          <Text style={style.buttonText}>Login</Text>
-        </TouchableOpacity>
+                <View style={style.buttonContainer}>
+                    <TouchableOpacity style={style.button} onPress={handleLogin}>
+                        <Text style={style.buttonText}>Login</Text>
+                    </TouchableOpacity>
 
-        <Text style={style.registerText} onPress={irParaRegistro}>
-          Não tem uma conta? <Text style={style.registerLink}>Cadastre-se</Text>
-        </Text>
-      </View>
-    </KeyboardAvoidingView>
-  );
+                    <Text style={style.registerText} onPress={irParaRegistro}>
+                        Não tem uma conta? <Text style={style.registerLink}>Cadastre-se</Text>
+                    </Text>
+                </View>
+            </View>
+        </KeyboardAvoidingView>
+    );
 };
 
 export default Login;
